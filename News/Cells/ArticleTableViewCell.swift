f257a88f1b29d64b7516797f96463ef1a14e4521
Michael <mrflowers22@uwalumni.com>
2021-11-13 15:32:09 -0600

    New file ArticleTableViewCell - attached IBOutlets for title and description

---
+++ News/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ArticleTableViewCell.swift
+//  News
+//
+//  Created by Michael Flowers on 11/13/21.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
