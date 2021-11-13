dd5e33c156e74bc180b84d781110aabf207d7b93
Michael <mrflowers22@uwalumni.com>
2021-11-13 08:21:42 -0600

    modified AppDelegate and NewsListTVClr and Plist.

--- News/Controllers/NewsListTableViewController.swift
+++ News/Controllers/NewsListTableViewController.swift
@@ -11,7 +11,7 @@ class NewsListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationController?.navigationBar.prefersLargeTitles = true
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
