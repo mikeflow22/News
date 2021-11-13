755030cc90f0719d11fe2efce6c4faaceea57e14
Michael <mrflowers22@uwalumni.com>
2021-11-13 16:00:03 -0600

    project fetches from api successfully

--- News/Controllers/NewsListTableViewController.swift
+++ News/Controllers/NewsListTableViewController.swift
@@ -8,44 +8,49 @@
 import UIKit
 
 class NewsListTableViewController: UITableViewController {
-
+    private var articleListVM: ArticleListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = true
         let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=2b823cb27e30476d9de6759791d488ca")!
         
-        WebService().getArticles(url: url) { _ in
-            
+        WebService().getArticles(url: url) {  articles in
+            if let articles = articles {
+                self.articleListVM = ArticleListViewModel(articles: articles)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
             
         }
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfRowsInSection(section)
     }
 
-    /*
+ 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as! ArticleTableViewCell
+        let article = articleListVM.articleAtIndex(indexPath.row)
+        
+        cell.titleLabel.text? = article.title
+        cell.descriptionLabel.text? = article.description ?? "No Description for article"
 
-        // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
--- News/View Models/ArticleViewModel.swift
+++ News/View Models/ArticleViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 //parent view model contains the array
 struct ArticleListViewModel {
+    //This is what we will use to set the tableView
     let articles: [Article]
 }
 
@@ -20,13 +21,14 @@ extension ArticleListViewModel {
         articles.count
     }
     
+    //this is to get the Model and put it in the tableViewCell
     func articleAtIndex(_ index: Int) -> ArticleViewModel {
         let article = articles[index]
         return ArticleViewModel(article)
     }
 }
 
-//represents one article
+//represents one article - for the tableViewCell
 struct ArticleViewModel {
     private let article: Article
 }
